8a34e4048f0e579e5fff9720a42e2735e26802a3
nuochka <anstskovalenko@gmail.com>
2025-03-31 20:43:14 +0200

    Added camera initialization and entry point for starting the application

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ import dotenv
 import spotify
 import sys
 
+import cv2
+from gestureControl import GestureControl
+
 from log import AppLogger
 from spotify import SpotifyHandler
 
@@ -16,6 +19,23 @@ if __name__ == "__main__":
     if not dotenv.load_dotenv(verbose=True):
         AppLogger.error("UNRECOVERABLE: Unable to access environmental variables. Exiting...")
         sys.exit(1)
+
+    cap = cv2.VideoCapture(0)
+    gesture_control = GestureControl()
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        frame = cv2.flip(frame, 1)
+        frame = gesture_control.process_frame(frame)
+        
+        cv2.imshow('Gesture Control', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
     AppLogger.debug("Succesfully initialized, entering the main application loop.")
     pass
