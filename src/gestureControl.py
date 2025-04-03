59a660d47e55e56f1137c82c72cc13cf6b758571
not-forest <notforest69@gmail.com>
2025-04-03 20:36:51 +0200

    Fixed loop bugs. Added better error handling control for the main loop and gesture recognition system.

--- src/gestureControl.py
+++ src/gestureControl.py
@@ -21,7 +21,11 @@ class GestureAction(Enum):
 
 class GestureControl:
     def __init__(self, action_cooldown=2, quick_action_cooldown=0.3):
-        self.hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
+        self.hands = mp_hands.Hands(
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7
+        )
+
         self.quick_action_cooldown = quick_action_cooldown
         self.action_cooldown = action_cooldown
         self.last_action_time = 0
@@ -36,27 +40,29 @@ class GestureControl:
     """ 
         Processes the currently obtained frame from the video camera.
     """
-    def process_frame(self, frame):
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(rgb_frame)
+    def process_frame(self, frame, debug=True):
+        self.current_state = GestureAction.NONE
         
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-                fingers = self.count_fingers(hand_landmarks)
-                
-                current_time = time.time()
-                if current_time - self.last_action_time > self.cooldown:
-                    if fingers == 1:
+        current_time = time.time()
+        if current_time - self.last_action_time > self.action_cooldown:
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = self.hands.process(rgb_frame)
+
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    fingers = self.count_fingers(hand_landmarks)
+                    
+                    self.last_action_time = current_time
+                    if fingers == 5: # Checking this possibility first.
+                        self.current_state = GestureAction.PLAY_PAUSE
+                        AppLogger.debug("Gesture detected: PLAY_PAUSE")
+                    elif fingers == 1:
                         self.current_state = GestureAction.NEXT_TRACK
                         AppLogger.debug("Gesture detected: NEXT_TRACK")
                     elif fingers == 2:
                         self.current_state = GestureAction.PREV_TRACK
                         AppLogger.debug("Gesture detected: PREV_TRACK")
-                    elif fingers == 5:
-                        self.current_state = GestureAction.PLAY_PAUSE
-                        AppLogger.debug("Gesture detected: PLAY_PAUSE")
-                    else:
-                        self.last_action_time = current_time
-                        AppLogger.debug("No valid gesture detected.")
+                    
+                    if debug:
+                        mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
         return frame
--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ from spotify import SpotifyHandler
 from gestureControl import GestureControl, GestureAction 
 
 CAM_TIMEOUT     = os.getenv("SPOTIFY_REMOTE_CAMERA_TIMEOUT", default="15")
-DBG_MODE        = os.getenv("SPOTIFY_REMOTE_DEBUG", default="True")
+DBG_MODE        = os.getenv("SPOTIFY_REMOTE_DEBUG_MODE", default="True")
 
 # Main application entry point.
 if __name__ == "__main__":
@@ -26,21 +26,22 @@ if __name__ == "__main__":
         AppLogger.fatal("UNRECOVERABLE: Unable to access environmental variables. Exiting...")
         sys.exit(1)
 
-    spot = SpotifyHandler()
 
     try:
-        while True:
-            cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(0)
+        spot = SpotifyHandler()
+        gesture_ctrl = GestureControl()
 
+        AppLogger.debug("Succesfully initialized, entering the main application loop.")
+        while True:
             while cap.isOpened():
-                gesture_ctrl = GestureControl()
                 ret, frame = cap.read()
                 if not ret:
                     AppLogger.warning("Lost communication with video camera.")
                     break
 
                 frame = cv2.flip(frame, 1)
-                frame = gesture_ctrl.process_frame(frame)
+                frame = gesture_ctrl.process_frame(frame, bool(DBG_MODE))
 
                 match gesture_ctrl.current_state:
                     case GestureAction.NEXT_TRACK:
@@ -56,6 +57,7 @@ if __name__ == "__main__":
 
                 if bool(DBG_MODE):  # Reference window is only available in debug mode.
                     cv2.imshow('Gesture Control', frame)
+                    cv2.waitKey(1)
 
             cap.release()
             if bool(DBG_MODE):
@@ -66,4 +68,4 @@ if __name__ == "__main__":
     except Exception as e:
         AppLogger.fatal(f'UNRECOVERABLE: Unhandled error occured: {e}')
     finally:
-        AppLogger.debug("Succesfully initialized, entering the main application loop.")
+        AppLogger.info("Stopping the spotify-remote daemon...")
